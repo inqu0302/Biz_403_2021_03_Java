2ef9587f6a850a24048f081c8960bcfc529387b5
inqu0302 <inqu0302@naver.com>
2021-03-17 10:04:08 +0900

    연산 복습

---
+++ Java_000_Hello/src/com/callor/hello/Numbers.java
@@ -0,0 +1,17 @@
+package com.callor.hello;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		
+		// 4칙연산과 나머지연산
+		System.out.println("33 + 2 = " + (33 + 2));
+		System.out.println("33 - 2 = " + (33 - 2));
+		System.out.println("33 * 2 = " + (33 * 2));
+		System.out.println("33 / 2 = " + (33 / 2));
+		System.out.println("33 % 2 = " + (33 % 2));
+		
+		
+	}
+
+}

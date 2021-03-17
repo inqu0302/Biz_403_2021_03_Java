686cc9bd2237a40fe1ccc2dc99ce2df3e7a42b20
inqu0302 <inqu0302@naver.com>
2021-03-17 16:38:36 +0900

    Varriable

---
+++ Java_001_Value/src/com/callor/values/var/Varriable_04.java
@@ -0,0 +1,41 @@
+package com.callor.values.var;
+
+public class Varriable_04 {
+
+	public static void main(String[] args) {
+		
+		// 변수가 초기화 된다(사용할 준비가 된다)
+		// 선언과 값의 할당을 동시에 수행
+		int dan = 9;
+		
+		
+		System.out.println("===========================");
+		System.out.print(dan);
+		System.out.println(" 단 구구단");
+		System.out.println("===========================");
+		
+		System.out.println(dan + " X 1 = " + (dan * 1));
+		System.out.println(dan + " X 2 = " + (dan * 2));
+		System.out.println(dan + " X 3 = " + (dan * 3));
+		System.out.println(dan + " X 4 = " + (dan * 4));
+		System.out.println(dan + " X 5 = " + (dan * 5));
+		System.out.println(dan + " X 6 = " + (dan * 6));
+		System.out.println(dan + " X 7 = " + (dan * 7));
+		System.out.println(dan + " X 8 = " + (dan * 8));
+		System.out.println(dan + " X 9 = " + (dan * 9));
+		
+		System.out.println("==========================");
+		
+		System.out.printf("%d X 1 = %d\n", dan, dan*1);
+		System.out.printf("%d X 2 = %d\n", dan, dan*2);
+		System.out.printf("%d X 3 = %d\n", dan, dan*3);
+		System.out.printf("%d X 4 = %d\n", dan, dan*4);
+		System.out.printf("%d X 5 = %d\n", dan, dan*5);
+		System.out.printf("%d X 6 = %d\n", dan, dan*6);
+		System.out.printf("%d X 7 = %d\n", dan, dan*7);
+		System.out.printf("%d X 8 = %d\n", dan, dan*8);
+		System.out.printf("%d X 9 = %d\n", dan, dan*9);
+		
+		
+	}
+}
